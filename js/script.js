5683be5fe0e2c4988133833be1a0125bd74802a0
Juniper James <vvlpespro@gmail.com>
2022-12-02 14:48:31 -0500

    lunar countdown functionality implemented

--- js/script.js
+++ js/script.js
@@ -37,25 +37,25 @@ const settings = {
 		if (response) {
 		  console.log(response);
 		  $phaseData.text(response.phaseName);
-		  $moonAge.text(
-			Math.floor(response.phaseAge) + " days since the new moon."
-		  );
+		  $moonAge.text(Math.floor(response.phaseAge) + " days since the new moon.");
 		}
 	  });
 	}
   
 	function handleGetTimeData() {
 	  $.ajax(timeURL).then(function (response) {
+		
 		$localTime.text("current time: " + response.current_time);
 		$moonTimes.text(
-		  "moonrise: " + response.moonrise + "  moonset: " + response.moonset
-		);
+		  "moonrise: " + response.moonrise + "  moonset: " + response.moonset);
 		$sunTimes.text(
-		  "sunrise : " + response.sunrise + " sunset: " + response.sunset
-		);
+		  "sunrise : " + response.sunrise + " sunset: " + response.sunset);
 		$distance.text(
-		  "lunar altitude: " + Math.floor(response.moon_distance) + "km"
-		);
+		  "lunar altitude: " + Math.floor(response.moon_distance) + "km");
+
+
+
+
 		  function toMilis(target) {
 			  let hh = parseInt(target.slice(0, 3));
 			  let mm = parseInt(target.slice(3, 5));
@@ -64,14 +64,15 @@ const settings = {
 			  let output = hh + mm;
 			  return output;
 			}
+
 		  function toStandard(target){
 			  let minutes = (target/1000)/60
 			  let hours = 	Math.floor(minutes/60)
-			  minutes = (minutes%60)* (60/100)
+			  minutes = Math.floor((minutes%60)* (60/100))
 			  return `${hours} hours and ${minutes} minutes`
 		  }
   
-			let miliDay = toMilis("24:00");
+		  let miliDay = toMilis("24:00");
 		  let miliLocal = toMilis(response.current_time);
 		  let miliMoonset = toMilis(response.moonset);
 		  let miliMoonrise = toMilis(response.moonrise);
@@ -79,9 +80,12 @@ const settings = {
 		  let miliSunrise = toMilis(response.sunrise);
 		  let sunMessage = "";
 		  let moonMessage = "";
-		  if (miliLocal > miliSunset) {
-			sunMessage =
-			  " time until sunrise = " + toStandard((miliDay - miliLocal) + miliSunrise);
+		  
+		 
+		 
+		 
+			if (miliLocal > miliSunset) {
+			sunMessage =  " time until sunrise = " + toStandard((miliDay - miliLocal) + miliSunrise);
 		  } else if (miliLocal < miliSunset && miliLocal > miliSunrise) {
 			sunMessage = "time until sunset = " +toStandard((miliSunset - miliLocal));
 		  } else if (miliLocal < miliSunrise) {
@@ -89,9 +93,37 @@ const settings = {
 		  }
 		  $sunTimes.text(sunMessage);
 		
-	  ;
-	  });
+			if (miliMoonrise < miliMoonset){
+				console.log('alpha')
+				if (miliLocal < miliMoonrise) {
+					moonMessage = "time until moonrise: " + toStandard(miliMoonrise-miliLocal)
+			} 	else if (miliLocal > miliMoonrise && miliLocal < miliMoonset){
+					moonMessage = "time until moonset: "+ toStandard(miliMoonset - miliLocal)
+			}
+				else if (miliLocal > miliMoonset){
+					moonMessage = "time until moonrise: " + (toStandard(miliDay - miliLocal)+miliMoonrise)
+			}
+			$moonTimes.text = moonMessage  
+			}
+			else if (miliMoonset < miliMoonrise){
+				console.log('beta')
+				if (miliLocal < miliMoonset){
+					moonMessage = "time until moonset: "+ toStandard(miliMoonset - miliLocal)
+				}
+				else if (miliLocal > miliMoonset && miliLocal < miliMoonrise){
+					moonMessage = "time until moonrise: "+ toStandard(miliMoonset - miliLocal)
+				}
+				else if (miliLocal>miliMoonrise){
+					moonMessage="time until moonset: "+toStandard((miliDay - miliLocal) + miliMoonset )
+				}
+				$moonTimes.text(moonMessage)  
+			}
+		 
+		})
 	}
+	  
+	  
+	
 	
 	
 	handleGetPhase();
